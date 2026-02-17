d5049ae643ed7c976788204cbe92f790723bc1dd
CODonnie <ordudesigns@gmail.com>
2026-02-17 12:45:49 +0100

    empty

--- src/admin/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isAdmin = localStorage.getItem("correctAdminCreds") === "true";
-  return isAdmin ? children : <Navigate to="/admin" replace />;
-};
-
-export default ProtectedRoute;
